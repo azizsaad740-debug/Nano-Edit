799ba4e3993d8e386ce7a48c92e36d42141ae791
[dyad] <git@dyad.sh>
2025-10-08 16:10:14 +0500

    [dyad] Adding keyboard shortcuts and tooltips - wrote 2 file(s)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,6 +2,11 @@ import { Image as ImageIcon, RotateCcw, Download, Eye, Copy } from "lucide-react
 import { Button } from "@/components/ui/button";
 import React from "react";
 import { ThemeToggle } from "./ThemeToggle";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 interface HeaderProps {
   onReset: () => void;
@@ -22,31 +27,59 @@ const Header = ({ onReset, onDownload, onCopy, hasImage, onTogglePreview, childr
       <div className="flex items-center gap-2">
         {children}
         <ThemeToggle />
-        <Button
-          variant="outline"
-          size="sm"
-          disabled={!hasImage}
-          onMouseDown={() => onTogglePreview(true)}
-          onMouseUp={() => onTogglePreview(false)}
-          onMouseLeave={() => onTogglePreview(false)}
-          onTouchStart={() => onTogglePreview(true)}
-          onTouchEnd={() => onTogglePreview(false)}
-        >
-          <Eye className="h-4 w-4 mr-2" />
-          Preview
-        </Button>
-        <Button variant="outline" size="sm" onClick={onReset} disabled={!hasImage}>
-          <RotateCcw className="h-4 w-4 mr-2" />
-          Reset
-        </Button>
-        <Button variant="outline" size="sm" onClick={onCopy} disabled={!hasImage}>
-          <Copy className="h-4 w-4 mr-2" />
-          Copy
-        </Button>
-        <Button size="sm" onClick={onDownload} disabled={!hasImage}>
-          <Download className="h-4 w-4 mr-2" />
-          Download
-        </Button>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              variant="outline"
+              size="sm"
+              disabled={!hasImage}
+              onMouseDown={() => onTogglePreview(true)}
+              onMouseUp={() => onTogglePreview(false)}
+              onMouseLeave={() => onTogglePreview(false)}
+              onTouchStart={() => onTogglePreview(true)}
+              onTouchEnd={() => onTogglePreview(false)}
+            >
+              <Eye className="h-4 w-4 mr-2" />
+              Preview
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Hold to see original image</p>
+          </TooltipContent>
+        </Tooltip>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button variant="outline" size="sm" onClick={onReset} disabled={!hasImage}>
+              <RotateCcw className="h-4 w-4 mr-2" />
+              Reset
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Reset all edits</p>
+          </TooltipContent>
+        </Tooltip>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button variant="outline" size="sm" onClick={onCopy} disabled={!hasImage}>
+              <Copy className="h-4 w-4 mr-2" />
+              Copy
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Copy to clipboard (Cmd+Shift+C)</p>
+          </TooltipContent>
+        </Tooltip>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button size="sm" onClick={onDownload} disabled={!hasImage}>
+              <Download className="h-4 w-4 mr-2" />
+              Download
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Download image (Cmd+S)</p>
+          </TooltipContent>
+        </Tooltip>
       </div>
     </header>
   );
