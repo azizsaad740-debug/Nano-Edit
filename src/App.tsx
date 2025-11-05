3d8aadb628c20550bb57f4fbc19e3a8813282822
azizsaad740-debug <azizsaad740@gmail.com>
2025-11-05 22:53:15 +0500

    [dyad] Configured all application routes in src/App.tsx. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React from "react";
-import { IndexPage } from "./pages/Index"; // FIX 123: Import named export
-import NotFound from "./pages/NotFound";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { IndexPage } from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Terms from "./pages/Terms";
+import Privacy from "./pages/Privacy";
+import { Community } from "./pages/Community";
+import AdminDashboard from "./pages/AdminDashboard";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<IndexPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/admin" element={<AdminDashboard />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
