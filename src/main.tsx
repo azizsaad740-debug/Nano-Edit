dbe7882cb8420d135b200e2941a7f62b1953ee9c
azizsaad740-debug <azizsaad740@gmail.com>
2025-11-05 20:22:25 +0500

    [dyad] Wrapped the application with SessionProvider to resolve the useSession context error. - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
+import { SessionProvider } from "./integrations/supabase/session-provider";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
@@ -9,6 +10,8 @@ if (!container) throw new Error("Failed to find the root element");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <App />
+    <SessionProvider>
+      <App />
+    </SessionProvider>
   </React.StrictMode>
 );
